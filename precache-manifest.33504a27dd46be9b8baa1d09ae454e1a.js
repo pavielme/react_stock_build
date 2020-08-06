e6dc5bc8d5d9f3d5430d061997086aaa605cac0f
Paviel Manthon Phadchoo <paviel@pop-os.localdomain>
2020-08-06 21:26:42 +0200

    react build

---
+++ precache-manifest.33504a27dd46be9b8baa1d09ae454e1a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fa18b1eb277ba85e90271c2cddfcaba",
+    "url": "/index.html"
+  },
+  {
+    "revision": "38113cccf921a594ce97",
+    "url": "/static/css/main.53c57a4d.chunk.css"
+  },
+  {
+    "revision": "84805af3d1c02e9940b3",
+    "url": "/static/js/2.67eda9d1.chunk.js"
+  },
+  {
+    "revision": "f65b45f562155f0fa2653b346e3552a6",
+    "url": "/static/js/2.67eda9d1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "38113cccf921a594ce97",
+    "url": "/static/js/main.4bff2662.chunk.js"
+  },
+  {
+    "revision": "7a59cd1eaa6a448dceb6",
+    "url": "/static/js/runtime-main.e4204574.js"
+  }
+]);
